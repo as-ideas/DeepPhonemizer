167ff836992c16386848c1481c7718db05003883
cschaefer26 <c.schaefer.home@gmail.com>
2021-04-11 09:26:57 +0200

    Refactoring phonemizer.

--- dp/phonemizer.py
+++ dp/phonemizer.py
@@ -10,33 +10,36 @@ class Phonemizer:
 
     def __init__(self,
                  checkpoint_path: str,
-                 punctuation='().,:?!',
-                 expand_acronyms=True,
                  lang_phoneme_dict: Dict[str, Dict[str, str]] = None) -> None:
         checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
         self.model = TransformerModel.from_config(checkpoint['config'])
         self.model.load_state_dict(checkpoint['model'])
         self.preprocessor = checkpoint['preprocessor']
         self.lang_phoneme_dict = lang_phoneme_dict
-        self.expand_acronyms = expand_acronyms
-        punctuation += ' '
-        self.punc_set = set(punctuation)
-        self.punc_pattern=re.compile(f'([{punctuation}])')
 
-    def __call__(self, text: str, lang: str) -> str:
-        words = re.split(self.punc_pattern, text)
-        if self.expand_acronyms:
+    def __call__(self,
+                 text: str,
+                 lang: str,
+                 punctuation='().,:?!',
+                 expand_acronyms=True) -> str:
+        punctuation += ' '
+        punc_set = set(punctuation)
+        punc_pattern=re.compile(f'([{punctuation}])')
+
+        text = ''.join([t for t in text if t.isalnum() or t in punc_set])
+        words = re.split(punc_pattern, text)
+        if expand_acronyms:
             words = [self.expand_acronym(w) for w in words]
 
         # collect dictionary phonemes for words and hyphenated words
-        word_phonemes = {word: self.get_dict_entry(word, lang) for word in words}
+        word_phonemes = {word: self.get_dict_entry(word, lang, punc_set) for word in words}
 
         # collect dictionary phonemes for subwords in hyphenated words
         word_splits = {w: w.split('-') for w in words if word_phonemes[w] is None}
         subwords = {w for values in word_splits.values() for w in values if len(w) > 0}
         for subword in subwords:
             if subword not in word_phonemes:
-                word_phonemes[subword] = self.get_dict_entry(subword, lang)
+                word_phonemes[subword] = self.get_dict_entry(subword, lang, punc_set)
 
         # predict all non-hyphenated words and all subwords of hyphenated words
         words_to_predict = []
@@ -70,8 +73,11 @@ class Phonemizer:
         phons = ''.join(pred_decoded)
         return phons
 
-    def get_dict_entry(self, word: str, lang: str) -> Union[str, None]:
-        if word in self.punc_set:
+    def get_dict_entry(self,
+                       word: str,
+                       lang: str,
+                       punc_set: set) -> Union[str, None]:
+        if word in punc_set:
             return word
         if not self.lang_phoneme_dict or lang not in self.lang_phoneme_dict:
             return None
@@ -108,6 +114,7 @@ class Phonemizer:
 if __name__ == '__main__':
     checkpoint_path = '../checkpoints/latest_model.pt'
     lang_phoneme_dict = {'de': {'E-Mail': 'ˈiːmeɪ̯l'}}
-    phonemizer = Phonemizer(checkpoint_path=checkpoint_path, lang_phoneme_dict=lang_phoneme_dict)
-    phons = phonemizer('Der E-Mail kleine SPD Prinzen-könig - Francesco Cardinale, pillert an seinem Pillermann.', lang='de')
+    phonemizer = Phonemizer(checkpoint_path=checkpoint_path,
+                            lang_phoneme_dict=lang_phoneme_dict)
+    phons = phonemizer('Der E-Mail kleine <SPD Prinzen-könig - Francesco Cardinale, pillert an seinem Pillermann.', lang='de')
     print(phons)
