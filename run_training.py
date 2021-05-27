4f19dbb38aa55a1a07ffa2ec362bedfa092a525f
cschaefer26 <c.schaefer.home@gmail.com>
2021-05-27 20:42:10 +0200

    Fixed training example.

--- run_training.py
+++ run_training.py
@@ -3,14 +3,16 @@ from dp.train import train
 
 if __name__ == '__main__':
 
-    train_data = [
-                     ('en_us', 'young', 'jʌŋ'),
-                     ('de', 'benützten', 'bənʏt͡stn̩'),
-                     ('de', 'gewürz', 'ɡəvʏʁt͡s')
-                 ] * 1000
+    train_data = [('en_us', 'young', 'jʌŋ'),
+                  ('de', 'benützten', 'bənʏt͡stn̩'),
+                  ('de', 'gewürz', 'ɡəvʏʁt͡s')] * 1000
+
+    val_data = [('en_us', 'young', 'jʌŋ'),
+                ('de', 'benützten', 'bənʏt͡stn̩')] * 100
 
     config_file = 'dp/configs/forward_config.yaml'
     preprocess(config_file=config_file,
                train_data=train_data,
+               val_data=val_data,
                deduplicate_train_data=False)
     train(config_file=config_file)
