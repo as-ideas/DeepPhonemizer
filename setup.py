f8c97b3d25117c34f319aada3fccbfbc91d08844
cschaefer26 <c.schaefer.home@gmail.com>
2021-06-18 16:30:44 +0200

    Remove python 10 support.

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
