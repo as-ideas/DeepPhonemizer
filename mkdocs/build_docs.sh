1d991d1d6331e0b78bd3d95addde53ebb06cc6fa
cschaefer26 <c.schaefer.home@gmail.com>
2021-06-22 15:09:45 +0200

    Fix build docs.

--- mkdocs/build_docs.sh
+++ mkdocs/build_docs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir ../docs
 cp ../README.md docs/index.md
 cp ../CONTRIBUTING.md docs/CONTRIBUTING.md
 cp ../LICENSE docs/LICENSE.md
 cp -R ../figures docs/
 python autogen.py
-mkdir ../docs
 mkdocs build -c -d ../docs/
