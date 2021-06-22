808c15742e4c1c97f39d9fb4e5030c3abedd93e3
cschaefer26 <c.schaefer.home@gmail.com>
2021-06-22 15:14:22 +0200

    Fix nonexisting build docs dir.

--- mkdocs/build_docs.sh
+++ mkdocs/build_docs.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir ../docs
+mkdir docs
 cp ../README.md docs/index.md
 cp ../CONTRIBUTING.md docs/CONTRIBUTING.md
 cp ../LICENSE docs/LICENSE.md
 cp -R ../figures docs/
 python autogen.py
+mkdir ../docs
 mkdocs build -c -d ../docs/
