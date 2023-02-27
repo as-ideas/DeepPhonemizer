c829fba8f61f3c9ba632b0b07784662326f778f9
Moritz Haller <mhaller@brave.com>
2023-02-27 09:10:27 +0100

    Move logging config into client code

--- dp/utils/logging.py
+++ dp/utils/logging.py
@@ -1,16 +1,4 @@
-import logging.config
-import logging.config
-import os
 from logging import Logger, getLogger
-from pathlib import Path
-
-import dp.configs
-from dp.utils.io import read_config
-
-main_dir = os.path.dirname(os.path.abspath(dp.configs.__file__))
-config_file_path = Path(main_dir) / 'logging.yaml'
-config = read_config(config_file_path)
-logging.config.dictConfig(config)
 
 
 def get_logger(name: str) -> Logger:
